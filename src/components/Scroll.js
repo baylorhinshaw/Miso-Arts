e02a1d47fcb69835fc1d0a16fa11717a9c48d91b
Baylor <baylorhinshaw21@gmail.com>
2022-04-04 14:18:53 -0400

    added images, component files, page files

---
+++ src/components/Scroll.js
@@ -0,0 +1,11 @@
+import HorizontalScroll from "react-scroll-horizontal";
+
+function Scroll() {
+    return (
+        <div className="Scroll">
+            
+        </div>
+    );
+}
+
+export default Scroll;
