35bd8b031ed973e150a030c5d26f8f9334592f51
Baylor <baylorhinshaw21@gmail.com>
2022-04-05 12:15:56 -0400

    git horizontal scroll to work

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,10 +1,25 @@
 import HorizontalScroll from "react-scroll-horizontal";
+import "../components/Main.css";
 
 function Scroll() {
+
+const child = { width: '300em', height: '100%' }
+
     return (
         <div className="Scroll">
             <HorizontalScroll>
-                
+                <div className='main'>
+                    <h1>Hello World 1</h1>
+                </div>
+                <div className='main'>
+                    <h1>Hello World 2</h1>
+                </div>
+                <div className='main'>
+                    <h1>Hello World 3</h1>
+                </div>
+                <div className='main'>
+                    <h1>Hello World 4</h1>
+                </div>
             </HorizontalScroll>
         </div>
     );
